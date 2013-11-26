1cd32bbb82c3103890355d2ed02ef87767fa034b
Skial Bainn <skial.bainn@gmail.com>
2013-11-26 21:58:39 +0000

    Possible progress.

--- src/uhx/macro/Yield.hx
+++ src/uhx/macro/Yield.hx
@@ -38,7 +38,9 @@ class Yield {
 		Yield.cls = cls;
 		
 		if (!Context.defined( 'display' )) for (field in fields) switch (field.kind) {
-			case FFun(method) if(method.expr != null): loop( method.expr, field );
+			case FFun(method) if (method.expr != null):
+				finalize( loop( method.expr, field.name ), method );
+				
 			case _:
 		}
 		
@@ -46,11 +48,13 @@ class Yield {
 	}
 	
 	public static var name:String = null;
+	public static var cases:Array<Expr> = null;
+	public static var counter:Int = 0;
 	
-	public static function loop(e:Expr, f:Field, ?t:TypeDefinition):TypeDefinition {
+	public static function loop(e:Expr, n:String, ?t:TypeDefinition, ?c:Array<Case>, ?h:Bool = false):TypeDefinition {
 		if (t == null) {
 			t = {
-				name: '${cls.name}_${f.name}',
+				name: '${cls.name}_$n',
 				pack: cls.pack,
 				pos: cls.pos,
 				meta: [],
@@ -62,29 +66,145 @@ class Yield {
 		}
 		
 		switch (e) {
-			case { expr: EBlock(block), pos: pos }:
-				trace( block.length );
+			case { expr: EBlock(exprs), pos: pos }:
+				var cases:Array<Case> = [];
+				var copy = exprs.copy();
+				var sacrifice = exprs.copy();
+				var plen = cases.length;
+				
+				for (expr in copy) {
+					
+					plen = cases.length;
+					loop( expr, n, t, cases, h );
+					
+					if (cases.length > plen) {
+						cases[plen] = transformCase( cases[plen], sacrifice.splice(0, Lambda.indexOf(sacrifice, expr) ) );
+					}
+					
+				}
+				
+				if (cases.length > 0) {
+					
+					t.fields.get( 'move' ).body( { expr: EBlock( [ { expr: ESwitch( macro state, cases, null ), pos: e.pos } ] ), pos: e.pos } );
+					
+					switch (cases[cases.length - 1].expr) {
+						case { expr: EBlock(es), pos: pos } :
+							es.pop();
+							es.push( macro state = -1 );
+							
+						case _:
+							
+					}
+					
+				}
 				
 			case macro @:yield return $expr:
-				trace( expr );
+				setupDef( t );
+				
+				var _exprs = [];
+				_exprs.push( macro state = -1 );
+				_exprs.push( macro current = $expr );
+				
+				
+				var _case = {
+					values: [macro 0],
+					guard: null,
+					expr: { expr: EBlock( _exprs ), pos: e.pos }
+				};
+				
+				var index = c.push( _case );
+				
+				_case.values = [macro $v { index - 1 } ];
+				_exprs.push( macro state = $v{ index } );
+				
+				h = true;
 				
 			case macro @:yield break:
+				setupDef( t );
+				h = true;
 				
-				
-			case macro var $ident:$type:
+			case macro var $ident:$type = $expr:
 				t.fields.push( {
 					name: ident.toString(),
 					access: [APublic],
 					kind: FVar( type ),
-					pos: f.pos,
+					pos: e.pos,
 				} );
 				
 			case _:
-				e.iter( function(ee) t = loop(ee, f, t) );
+				e.iter( function(ee) t = loop(ee, n, t, c, h) );
 				
 		}
 		
 		return t;
+	}
+	
+	public static function setupDef(t:TypeDefinition) {
+		for (method in ['new', 'hasNext', 'next', 'move']) if (!t.fields.exists( method )) {
+			t.fields.push( method.mkField().mkPublic().toFFun().body( macro { } ) );
+		}
+		
+		if (!t.fields.exists( 'state' )) t.fields.push( 'state'.mkField().mkPublic().toFVar( macro :Int ) );
+		if (!t.fields.exists( 'current' )) t.fields.push( 'current'.mkField().mkPublic().toFVar( macro :Dynamic ) );
+		if (!t.fields.exists( 'iterator' )) t.fields.push( 'iterator'.mkField().mkPublic().toFFun().body( macro return this ) );
+		
+	}
+	
+	public static function transformCase(c:Case, chunks:Array<Expr>):Case {
+		switch (c.expr.expr) {
+			case EBlock(exprs):
+				chunks.shift();
+				
+				// I have to loop through looking for var's and hope it has already been lifted.
+				for (chunk in chunks) switch (chunk) {
+					case macro var $ident:$type = $expr:
+						var _tmp = macro $i { ident } = $expr;
+						chunk.expr = _tmp.expr;
+						
+					case _:
+				}
+				
+				c.expr.expr = EBlock( [ exprs.shift() ].concat( chunks ).concat( exprs ) );
+				
+			case _:
+		}
+		
+		return c;
+	}
+	
+	public static function finalize(t:TypeDefinition, f:Function) {
+		var result = false;
+		
+		if (t.fields.exists( 'move' )) switch (t.fields.get( 'move' ).kind) {
+			case FFun(m): 
+				switch (m.expr.expr) {
+					case EBlock(exprs) if (exprs.length > 0):
+						t.fields.get( 'new' ).body( macro { state = 0; } );
+						var copy = exprs.copy();
+						t.fields.get( 'next' ).ret( f.ret );
+						
+						copy.push( macro return current );
+						m.expr.expr = EBlock( copy );
+						t.fields.get( 'next' ).body( macro { return move(); } );
+						t.fields.get( 'hasNext' ).body( macro return state != -1 ).ret( macro:Bool );
+						
+						Context.defineType( t );
+						
+						f.expr = Context.parse( 'return new ${t.path()}()', f.expr.pos );
+						
+						trace( t.printTypeDefinition() );
+						trace( f );
+					case _:
+						
+						
+				}
+				
+			case _:
+				
+				
+		}
+		
+		return result;
 	}
 	
 }
